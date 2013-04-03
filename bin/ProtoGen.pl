af1092d6ae512f3fdd84f27459052603a8b526be
S.Lohse <steffen.lohse@neusoft.com>
2013-04-03 14:26:21 +0200

    preparations for using ndslib.dll from TempoGiusto

---
+++ bin/ProtoGen.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+
+use 5.006_000;
+use strict;    
+
+use File::stat;
+use File::Basename;
+use File::Path;
+use File::Spec;
+use Cwd;
+use Env;
+
+{
+   my $argc = $#ARGV+1; 
+
+   $argc==2 || die "Error: This script needs 2 parameters. Invocation: protobuf.pl D:\\some\\input\\folder\\with\\proto\\files D:\\some\\output\\folder\n";
+   
+   my $indir  = Cwd::abs_path($ARGV[0]);
+   my $outdir = $ARGV[1];
+   if(! -d $outdir)
+   {
+      mkpath($outdir);
+      (-d $outdir) || die "Error: Cannot create directory $outdir\n";
+   }
+   $outdir = Cwd::abs_path($outdir);
+
+   my $protoc_exe;
+   if($ENV{'OS'} eq "Windows_NT")
+   {
+      $protoc_exe   = "protoc.exe --error_format=msvs"; # todo: add this option for VS builds only, not for MonoDevelop
+   }
+   else
+   {
+      $protoc_exe   = "./protoc";
+   }
+
+   opendir(DIR, $indir) or die "Error: Cannot open input directory $indir\n";
+   while(my $file1 = readdir(DIR))
+   {
+      my ($name1, $path1, $suffix1) = fileparse($file1, qr{\.proto$});
+      if($suffix1 eq ".proto")
+      {
+         my $protofile = File::Spec->catfile($indir,  $name1.".proto");
+         my $hfile     = File::Spec->catfile($outdir, $name1.".pb.h");     
+         my $cppfile   = File::Spec->catfile($outdir, $name1.".pb.cc");     
+
+         my $prototimestamp = stat($protofile)->mtime;
+         my $cpptimestamp   = $prototimestamp;
+         my $htimestamp     = $prototimestamp;
+         if(-e $cppfile)
+         {
+            $cpptimestamp = stat($cppfile)->mtime;
+         }
+         if(-e hfile)
+         {
+            $htimestamp = stat($hfile)->mtime;
+         }
+
+         if( ($prototimestamp>=$cpptimestamp) || ($prototimestamp>=$htimestamp) )
+         {
+            print "Running protobuf code generator $protofile -> $cppfile\n";
+
+            my $cmd = $protoc_exe . " --proto_path=$indir $protofile --cpp_out=$outdir 2>&1";                
+            #print "$cmd\n";
+            my $result = `$cmd`;
+            $result eq "" || print "$result\n";
+
+            (-e $cppfile) || die "Error creating $cppfile\n";
+            (-e $hfile)   || die "Error creating $hfile\n";
+            my $cpptimestamp_new = stat($cppfile)->mtime;
+            my $htimestamp_new   = stat($hfile)->mtime;
+            ($cpptimestamp_new > $cpptimestamp) || die "Error creating $cppfile\n";
+            ($htimestamp_new   > $htimestamp)   || die "Error creating $hfile\n";
+            #print "Successfully created $cppfile\n";
+         }
+         else
+         {
+            #print "Code generation from $protofile is not necessary, destination file $cppfile is up to date.\n";
+         }
+      }
+   }
+}

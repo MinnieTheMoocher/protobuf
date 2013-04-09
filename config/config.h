534ea4b488139fd45aa6785761374641a4ace314
S.Lohse <steffen.lohse@neusoft.com>
2013-04-09 11:17:57 +0200

    make buildable for Win32 and Linux

--- config/config.h
+++ config/config.h
@@ -1,26 +1,177 @@
-/* the location of <hash_map> */
-#define HASH_MAP_H <hash_map>
-
-/* the namespace of hash_map/hash_set */
 #ifdef _MSC_VER
-	#if _MSC_VER<1310 || _MSC_VER>=1600
-		#define HASH_NAMESPACE std
-	#else
-		#define HASH_NAMESPACE stdext
-	#endif
+
+    /* the location of <hash_map> */
+    #define HASH_MAP_H <hash_map>
+
+    /* the namespace of hash_map/hash_set */
+    #if _MSC_VER<1310 || _MSC_VER>=1600
+       #define HASH_NAMESPACE std
+    #else
+        #define HASH_NAMESPACE stdext
+    #endif
+
+    /* the location of <hash_set> */
+    #define HASH_SET_H <hash_set>
+
+    /* define if the compiler has hash_map */
+    #define HAVE_HASH_MAP 1
+
+    /* define if the compiler has hash_set */
+    #define HAVE_HASH_SET 1
+
+    /* define if you want to use zlib.  See readme.txt for additional
+     * requirements. */
+    // #define HAVE_ZLIB 1
+
 #else
-   #error port me!
+
+    /* the name of <hash_map> */
+    #undef HASH_MAP_CLASS
+
+    /* the location of <unordered_map> or <hash_map> */
+    #undef HASH_MAP_H
+
+    /* the namespace of hash_map/hash_set */
+    #undef HASH_NAMESPACE
+
+    /* the name of <hash_set> */
+    #undef HASH_SET_CLASS
+
+    /* the location of <unordered_set> or <hash_set> */
+    #undef HASH_SET_H
+
+    /* Define to 1 if you have the <dlfcn.h> header file. */
+    #undef HAVE_DLFCN_H
+
+    /* Define to 1 if you have the <fcntl.h> header file. */
+    #undef HAVE_FCNTL_H
+
+    /* Define to 1 if you have the `ftruncate' function. */
+    #undef HAVE_FTRUNCATE
+
+    /* define if the compiler has hash_map */
+    #undef HAVE_HASH_MAP
+
+    /* define if the compiler has hash_set */
+    #undef HAVE_HASH_SET
+
+    /* Define to 1 if you have the <inttypes.h> header file. */
+    #undef HAVE_INTTYPES_H
+
+    /* Define to 1 if you have the <limits.h> header file. */
+    #undef HAVE_LIMITS_H
+
+    /* Define to 1 if you have the <memory.h> header file. */
+    #undef HAVE_MEMORY_H
+
+    /* Define to 1 if you have the `memset' function. */
+    #undef HAVE_MEMSET
+
+    /* Define to 1 if you have the `mkdir' function. */
+    #undef HAVE_MKDIR
+
+    /* Define if you have POSIX threads libraries and header files. */
+    #define HAVE_PTHREAD 1
+
+    /* Define to 1 if you have the <stdint.h> header file. */
+    #undef HAVE_STDINT_H
+
+    /* Define to 1 if you have the <stdlib.h> header file. */
+    #undef HAVE_STDLIB_H
+
+    /* Define to 1 if you have the `strchr' function. */
+    #undef HAVE_STRCHR
+
+    /* Define to 1 if you have the `strerror' function. */
+    #undef HAVE_STRERROR
+
+    /* Define to 1 if you have the <strings.h> header file. */
+    #undef HAVE_STRINGS_H
+
+    /* Define to 1 if you have the <string.h> header file. */
+    #undef HAVE_STRING_H
+
+    /* Define to 1 if you have the `strtol' function. */
+    #undef HAVE_STRTOL
+
+    /* Define to 1 if you have the <sys/stat.h> header file. */
+    #undef HAVE_SYS_STAT_H
+
+    /* Define to 1 if you have the <sys/types.h> header file. */
+    #undef HAVE_SYS_TYPES_H
+
+    /* Define to 1 if you have the <unistd.h> header file. */
+    #undef HAVE_UNISTD_H
+
+    /* Enable classes using zlib compression. */
+    #undef HAVE_ZLIB
+
+    /* Define to the sub-directory in which libtool stores uninstalled libraries.
+       */
+    #undef LT_OBJDIR
+
+    /* Name of package */
+    #undef PACKAGE
+
+    /* Define to the address where bug reports for this package should be sent. */
+    #undef PACKAGE_BUGREPORT
+
+    /* Define to the full name of this package. */
+    #undef PACKAGE_NAME
+
+    /* Define to the full name and version of this package. */
+    #undef PACKAGE_STRING
+
+    /* Define to the one symbol short name of this package. */
+    #undef PACKAGE_TARNAME
+
+    /* Define to the home page for this package. */
+    #undef PACKAGE_URL
+
+    /* Define to the version of this package. */
+    #undef PACKAGE_VERSION
+
+    /* Define to necessary symbol if this constant uses a non-standard name on
+       your system. */
+    #undef PTHREAD_CREATE_JOINABLE
+
+    /* Define to 1 if you have the ANSI C header files. */
+    #undef STDC_HEADERS
+
+    /* Enable extensions on AIX 3, Interix.  */
+    #ifndef _ALL_SOURCE
+    # undef _ALL_SOURCE
+    #endif
+    /* Enable GNU extensions on systems that have them.  */
+    #ifndef _GNU_SOURCE
+    # undef _GNU_SOURCE
+    #endif
+    /* Enable threading extensions on Solaris.  */
+    #ifndef _POSIX_PTHREAD_SEMANTICS
+    # undef _POSIX_PTHREAD_SEMANTICS
+    #endif
+    /* Enable extensions on HP NonStop.  */
+    #ifndef _TANDEM_SOURCE
+    # undef _TANDEM_SOURCE
+    #endif
+    /* Enable general extensions on Solaris.  */
+    #ifndef __EXTENSIONS__
+    # undef __EXTENSIONS__
+    #endif
+
+
+    /* Version number of package */
+    #undef VERSION
+
+    /* Define to 1 if on MINIX. */
+    #undef _MINIX
+
+    /* Define to 2 if the system does not provide POSIX.1 features except with
+       this defined. */
+    #undef _POSIX_1_SOURCE
+
+    /* Define to 1 if you need to in order for `stat' and other things to work. */
+    #undef _POSIX_SOURCE
+
+
 #endif
-
-/* the location of <hash_set> */
-#define HASH_SET_H <hash_set>
-
-/* define if the compiler has hash_map */
-#define HAVE_HASH_MAP 1
-
-/* define if the compiler has hash_set */
-#define HAVE_HASH_SET 1
-
-/* define if you want to use zlib.  See readme.txt for additional
- * requirements. */
-// #define HAVE_ZLIB 1
